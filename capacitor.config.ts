e78129503e911cbcd0cda117a00175fc06dca293
jouaspiazu <joeaspiazu@hotmail.com>
2024-07-24 19:03:25 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'movieIonicNg17App',
+  webDir: 'www'
+};
+
+export default config;
